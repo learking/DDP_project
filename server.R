01705d20e50fb1f530773c2ead8544468071d842
Kuangyu Wang <lear@Kuangyus-Mac-mini.local>
2015-06-21 15:43:09 -0700

    course app completed

--- server.R
+++ server.R
@@ -1,20 +1,50 @@
+#source("helper.R")
+
+library(shiny)
+#library(glmnet)
+#library(caret)
+#data(GermanCredit)
+
+load("german.rda")
+load("cmList.rda")
+
+# numVarVec = colnames(GermanCredit)[sapply(GermanCredit, is.numeric)]
+# numVarVec = numVarVec[sapply(numVarVec, function(x) ifelse(length(unique(GermanCredit[,x])) < 10,TRUE,FALSE))]
+# 
+# indexes = sample(1:nrow(GermanCredit), size=0.5*nrow(GermanCredit)) # Random sample of 50% of row numbers created
+# Train50 <- GermanCredit[indexes,] # Training data contains created indices
+# Test50 <- GermanCredit[-indexes,] # Test data contains the rest
+# 
+# excludeVar <- names(GermanCredit) %in% c("Class")  
+# cvFit = cv.glmnet(data.matrix(Train50[!excludeVar]), Train50$Class, alpha=1, standardize=FALSE, family='binomial',nfolds =5)
+# Y_prob = predict(cvFit, newx = data.matrix(Test50[!excludeVar]), type = "response",s=cvFit$lambda.min)
+# 
+# percentVec = seq(0.1,0.9,0.1)
+# 
+# getCM <- function(probVec, percentCutoff, trueL){
+#   cutOffV = quantile(probVec, percentCutoff)
+#   predL = ifelse(probVec > cutOffV, "Bad", "Good")
+#   return(confusionMatrix(predL, trueL)$table)
+# }
+# 
+# cmList = lapply(percentVec, function(x) getCM(Y_prob, x, Test50$Class))
+# names(cmList) = percentVec
+
+
+
 shinyServer(function(input, output, session) {
 
-  # Combine the selected variables into a new data frame
-  selectedData <- reactive({
-    iris[, c(input$xcol, input$ycol)]
+  output$histPlot = renderPlot({
+    mosaicplot(table(GermanCredit[,input$numVarSelected], GermanCredit$Class), main = "Mosaic plot: categorical variable VS good/bad class label", xlab = input$numVarSelected, ylab = "Class")
   })
-
-  clusters <- reactive({
-    kmeans(selectedData(), input$clusters)
+  
+  output$mytable = renderDataTable({
+    GermanCredit
+  }, options = list(lengthMenu = c(5, 30, 50), pageLength = 5))
+  
+  output$cmPlot = renderPlot({
+      fourfoldplot(cmList[[input$checkGroup]], color = c("#CC6666", "#99CC99"),
+                   conf.level = 0, margin = 1, main = "Confusion Matrix")  
   })
-
-  output$plot1 <- renderPlot({
-    par(mar = c(5.1, 4.1, 0, 1))
-    plot(selectedData(),
-         col = clusters()$cluster,
-         pch = 20, cex = 3)
-    points(clusters()$centers, pch = 4, cex = 4, lwd = 4)
-  })
-
+  
 })
--- ui.R
+++ ui.R
@@ -1,13 +1,62 @@
-shinyUI(pageWithSidebar(
-  headerPanel('Iris k-means clustering'),
-  sidebarPanel(
-    selectInput('xcol', 'X Variable', names(iris)),
-    selectInput('ycol', 'Y Variable', names(iris),
-                selected=names(iris)[[2]]),
-    numericInput('clusters', 'Cluster count', 3,
-                 min = 1, max = 9)
-  ),
-  mainPanel(
-    plotOutput('plot1')
-  )
+library(shiny)
+
+shinyUI(navbarPage("Develop Data Product Course Project",
+tabsetPanel(id="allTabs",                 
+    tabPanel("Explore",
+             
+             h4(a("The data set comes from UCI Machine Learning Repository", href="https://archive.ics.uci.edu/ml/datasets/Statlog+(German+Credit+Data)")),
+             h4("When a bank receives a loan application, based on the applicant’s profile the bank has to make a decision regarding whether to go ahead with the loan approval or not."),
+             h4("There are two types of customers: Good (pay on time and do not charge off) VS Bad (do not pay on time and/or charge off)."),
+             h4("Mosaic plot is a way to visualize relationships between different variables. Here we can investigate the relationship between 'Good/Bad label' variable and other categorical variables:"),
+             
+             selectInput("numVarSelected", 
+                         label = "Choose a variable to generate Mosaic plot",
+                         choices = c(
+                           "InstallmentRatePercentage"              ,"ResidenceDuration"                      ,"NumberExistingCredits"                 
+                           ,"NumberPeopleMaintenance"                ,"Telephone"                              ,"ForeignWorker"                         
+                           ,"CheckingAccountStatus.lt.0"             ,"CheckingAccountStatus.0.to.200"         ,"CheckingAccountStatus.gt.200"          
+                           ,"CheckingAccountStatus.none"             ,"CreditHistory.NoCredit.AllPaid"         ,"CreditHistory.ThisBank.AllPaid"        
+                           ,"CreditHistory.PaidDuly"                 ,"CreditHistory.Delay"                    ,"CreditHistory.Critical"                
+                           ,"Purpose.NewCar"                         ,"Purpose.UsedCar"                        ,"Purpose.Furniture.Equipment"  
+                           ),
+                         selected = "InstallmentRatePercentage"),
+             plotOutput("histPlot")
+    ),
+
+    tabPanel("Solution",                   
+             
+             h4("Logistic regression is built upon training data. The probabilities are computed for testing data. The modeling part of code is shown below:"),
+             
+             h4(code("Train50 <- GermanCredit[indexes,] # Training data contains created indices")),
+             h4(code("Test50 <- GermanCredit[-indexes,] # Test data contains the rest")),             
+             h4(code("cvFit = cv.glmnet(data.matrix(Train50[!excludeVar]), Train50$Class, alpha=1, standardize=FALSE, family='binomial',nfolds =5)")), 
+             h4(code("Y_prob = predict(cvFit, newx = data.matrix(Test50[!excludeVar]), type = 'response',s=cvFit$lambda.min)")),
+
+             h4("If you accept all applicants, the acceptance rate is 100%. If you reject all applicants, the acceptance rate is 0%. Anywhere in between, you can play with the widget below:"),
+             
+             sidebarLayout(
+               sidebarPanel( 
+                 radioButtons("checkGroup", label = h3("Please choose acceptance percentage to compare confusion matrix"), 
+                                c(
+                                  "10 %" = "0.1", 
+                                  "20 %" = "0.2", 
+                                  "30 %" = "0.3",
+                                  
+                                  "40 %" = "0.4", 
+                                  "50 %" = "0.5", 
+                                  "60 %" = "0.6",
+                                  
+                                  "70 %" = "0.7", 
+                                  "80 %" = "0.8", 
+                                  "90 %" = "0.9"
+                                  ), selected="0.9")
+             ),
+             
+             mainPanel(
+               tabPanel("Plot", plotOutput("cmPlot"))             
+               )
+             
+             )
+    )
+)
 ))
